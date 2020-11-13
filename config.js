dd4bd7806a82bd25ac7652ed419041bcf03c0a62
Slyai <47918720+Slyai@users.noreply.github.com>
2020-11-13 11:44:06 +0100

    V0.9.0

---
+++ config.js
@@ -0,0 +1,20 @@
+/* Ce fichier sert de config au projet vous pouvez modifier les parti qui sont entre '' */
+var ip = '192.168.1.4'; /* POUR MODIFIER L'IP */
+/* pour modifier les nom des relais */
+let allRelais = 'Tout les relais';
+let relais1 = 'Relais 1';
+let relais2 = 'Relais 2';
+let relais3 = 'Relais 3';
+let relais4 = 'Relais 4';
+let relais5 = 'Relais 5';
+let relais6 = 'Relais 6';
+let relais7 = 'Relais 7';
+let relais8 = 'Relais 8';
+
+let nameButtonOn = 'on';
+let nameButtonOff = 'off';
+
+/* Cette Partie est reservez aux dev merci DE NE PAS LA MODIFIER*/
+
+let ver = 'V0.9.0';
+var time = '200';
