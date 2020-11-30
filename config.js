e724c0fb089d6f1c2457423816f3211a887a59ab
Slyai <47918720+Slyai@users.noreply.github.com>
2020-11-30 21:13:52 +0100

    V1.0.0
    Release

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
+/* 
+	Auteur du projet : slyai
+	Version : 1.0.0
+	Copyright : apache licence 2.0
+*/
+
 /* Ce fichier sert de config au projet vous pouvez modifier les parti qui sont entre '' */
 var ip = '192.168.1.4'; /* POUR MODIFIER L'IP */
+/* titre du site */
+let titreSite = 'Baudimax';
 /* pour modifier les nom des relais */
 let allRelais = 'Tout les relais';
 let relais1 = 'Relais 1';
@@ -10,12 +18,15 @@ let relais5 = 'Relais 5';
 let relais6 = 'Relais 6';
 let relais7 = 'Relais 7';
 let relais8 = 'Relais 8';
-
+/* la partie en dessous permet de modifier le texte du tableau */
+let nomTable1 ='Nom';
+let nomTable2 ='Ouvrir';
+let nomTable3 ='Fermer';
 let nameButtonOn = 'on';
 let nameButtonOff = 'off';
 
 /* Cette Partie est reservez aux dev merci DE NE PAS LA MODIFIER*/
 
-let ver = 'V0.9.5';
-var time = '150';
+let ver = 'V1.0.0';
+let time = '150';
 let texteB = 'ip configuré :';
